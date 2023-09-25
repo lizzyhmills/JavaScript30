a376a4fa1f7495065fc75db0f4b49d6f3443511f
Lizzy Mills <lizzyhmills@gmail.com>
2023-09-25 14:26:47 +0100

    Slight refactoring

--- 01 - JavaScript Drum Kit/application.js
+++ 01 - JavaScript Drum Kit/application.js
@@ -1,8 +1,3 @@
-let keys = document.getElementsByClassName("key")
-console.log(keys)
-const sounds = document.querySelectorAll("audio")
-console.log(sounds[0].dataset.key)
-
 window.addEventListener('keydown', function(e) {
   let audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
   let key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
